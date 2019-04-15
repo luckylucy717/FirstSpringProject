358a81d8ca604cf93b815e5f429c3e6f3ed414bd
이해빈 <luckylucy0717@gmail.com>
2019-04-15 16:08:26 +0900

    index.jsp추가, servlet변경

--- src/main/java/com/company/first/HomeController.java
+++ src/main/java/com/company/first/HomeController.java
@@ -32,7 +32,7 @@ public class HomeController {
 		String formattedDate = dateFormat.format(date);
 		
 		model.addAttribute("serverTime", formattedDate );
-		
+		logger.info("log남기기~");
 		return "home";
 	}
 	
